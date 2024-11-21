29fc4faf294c0f85c0fced8eaf1c5fae99ac6963
ShedrachIkenna <Shedrach686@gmail.com>
2024-11-21 23:08:04 +0100

    Created post request test endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, request
 from langchain_community.llms import Ollama 
 
 app = Flask(__name__)
@@ -8,6 +8,16 @@ app = Flask(__name__)
 #response = llm.invoke("Tell me a soccer joke")
 
 #print(response)
+
+@app.route("/ai", methods=["POST"])
+def aiPost():
+    print("Post /ai called")
+    json_content = request.json
+    query = json_content.get("query")
+    print(f"query: {query}")
+
+    response_answer = "Sample response"
+    return response_answer
 
 def start_app():
     app.run(host="0.0.0.0", port=8080, debug=True)
