749de42893708993ba0644e1936f28e96f52ba83
ShedrachIkenna <Shedrach686@gmail.com>
2024-11-22 21:11:27 +0100

    tested '/pdf' endpoint and its working

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask, request
-from langchain_community.llms import Ollama 
+from flask import Flask, request, jsonify
+from langchain_ollama import OllamaLLM
 
 app = Flask(__name__)
 
-#llm = Ollama(model='llama3')
+cached_llm = OllamaLLM(model='llama3')
 
-#response = llm.invoke("Tell me a soccer joke")
-
-#print(response)
 
 @app.route("/ai", methods=["POST"])
 def aiPost():
@@ -16,8 +13,22 @@ def aiPost():
     query = json_content.get("query")
     print(f"query: {query}")
 
-    response_answer = "Sample response. query: " + query
+    response = cached_llm.invoke(query)
+    print(response)
+
+    response_answer = {"answer": response}
     return response_answer
+
+@app.route("/pdf", methods=["POST"])
+def pdfPost():
+    file = request.files["file"]
+    file_name = file.filename
+    save_file = "pdf/" + file_name
+    file.save(save_file)
+    print(f"filename: {file_name}")
+
+    response = {'status': 'Sucessfully uploaded file', 'filename': file_name}
+    return response
 
 def start_app():
     app.run(host="0.0.0.0", port=8080, debug=True)
