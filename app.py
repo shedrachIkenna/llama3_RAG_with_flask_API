dd653867915861b8b4f6df7d86b96ae4c72e912c
ShedrachIkenna <Shedrach686@gmail.com>
2024-11-23 02:41:17 +0100

    pdfloader, pdfreader and splitting pdf into chunks

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask, request, jsonify
 from langchain_ollama import OllamaLLM
+from langchain_community.vectorstores import Chroma 
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from langchain_community.document_loaders import PDFPlumberLoader
+
 
 app = Flask(__name__)
 
+folder_path = "db"
+
 cached_llm = OllamaLLM(model='llama3')
+
+embedding = FastEmbedEmbeddings()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1024, chunk_overlap=80, length_function=len, is_separator_regex=False
+)
+
 
 
 @app.route("/ai", methods=["POST"])
@@ -27,7 +41,26 @@ def pdfPost():
     file.save(save_file)
     print(f"filename: {file_name}")
 
-    response = {'status': 'Sucessfully uploaded file', 'filename': file_name}
+    loader = PDFPlumberLoader(save_file)
+    docs = loader.load_and_split()
+    print(f"docs length = {len(docs)}")
+
+    chunks = text_splitter.split_documents(docs)
+    print(f"chunks length={len(chunks)}")
+
+    vector_store = Chroma.from_documents(
+        documents=chunks, embedding=embedding, persist_directory=folder_path
+    )
+
+    vector_store.persist()
+
+    response = {
+        'status': 'Sucessfully uploaded file', 
+        'filename': file_name, 
+        "doc length": len(docs), 
+        "chunk length": len(chunks)
+    }
+
     return response
 
 def start_app():
